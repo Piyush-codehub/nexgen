5c8aea31a7f0bc614a7f79e63901f622bb9fad2c
Piyush-codehub <Piyushkumar01.tech@gmail.com>
2026-01-22 00:59:19 +0530

    Update headerfooter.js

--- headerfooter.js
+++ headerfooter.js
@@ -1,7 +1,8 @@
-fetch("headerfooter/header.html")
+fetch("header.html")
   .then(res => res.text())
   .then(data => document.getElementById("header").innerHTML = data);
 
-fetch("headerfooter/footer.html")
+fetch("footer.html")
   .then(res => res.text())
   .then(data => document.getElementById("footer").innerHTML = data);
+
